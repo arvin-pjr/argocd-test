4bfcb9a3f4c43ef44ae514fc0c25a87aa2c37344
aravind.poojari <aravind.poojari@aux.dkatalis.com>
2023-02-23 00:39:16 +0530

    updated

--- charts/uptime/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket   = "terraform-state-778785141721-eu-central-1-env-dev"
-    encrypt  = true
-    key      = "workloads/eks-cluster-bb-dev/uptime-kuma/terraform.tfstate"
-    region   = "eu-central-1"
-    role_arn = "arn:aws:iam::778785141721:role/terraform_backend"
-  }
-}
